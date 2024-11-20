51b4ff8ef4e01c30488ea530a1e07b3880d17c7e
kaziSariaRahman <sariarahaman@gmail.com>
2024-11-21 00:10:35 +0600

    Created logout in views.py

--- main/urls.py
+++ main/urls.py
@@ -19,8 +19,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('logout/', views.user_logout, name='logout'),
-    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),#logout
+    path('login/', views.user_login, name='login'),#login
+
 
 ]
 
