77654d6c0cb9b3a3be86d30ea0f6aaac7f6c8f3e
oxishtiak <22101044@uap-bd.edu.com>
2024-11-20 23:51:11 +0600

    created function add children

--- children/views.py
+++ children/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render, redirect
+
+
+
+
+
+
+@login_required
+def add_Child(request):
+    if request.method == "POST":
+        form = ChildForm(request.POST, request.FILES)
+        if form.is_valid():
+            child = form.save(commit)
+            child.user = request.user
+            child.save()
+            return render("success")
+    else:
+        form = childForm()
+    return render(request, "main/add_child.html", {"form": form})
+
+
