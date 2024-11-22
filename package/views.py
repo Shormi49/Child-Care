9adf9630cb4e330ae73de6525b2ae4ca8a695993
Shormi49 <siamahoque@gmail.com>
2024-11-23 01:48:22 +0600

    added add_package function and also create package model

--- package/views.py
+++ package/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+
+from django.contrib.auth.decorators import login_required
+
+from .forms import PackageForm
+from .models import Package
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+@login_required
+def add_package(request):
+    if request.method == "POST":
+        form = PackageForm(request.POST)
+        if form.is_valid():
+            package = form.save()
+            package.user = request.user
+            package.save()
+            return redirect("waiting_for_admin")
+    else:
+        form = PackageForm()
+    return render(request, "#", {"form": form})
