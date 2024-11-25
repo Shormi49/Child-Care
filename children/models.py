c17414d9e19887844ce352491e9f1e049a882fef
ISHTIAK AHMED SABBIR <120561198+oxishtiak@users.noreply.github.com>
2024-11-25 10:49:49 +0600

    define child class

--- children/models.py
+++ children/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Child(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="children")
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='children_images/')
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
