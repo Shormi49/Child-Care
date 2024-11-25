a00759eede4cf661ff026e071e30b6ccd1dbe948
oxishtiak <22101044@uap-bd.edu.com>
2024-11-25 10:42:49 +0600

    deleted

--- children/forms.py
+++ children/forms.py
@@ -1,8 +1,1 @@
-from django import forms
-from .models import  Child
 
-
-class ChildForm(forms.ModelForm):
-    class Meta:
-        model = Child
-        fields = ['name', 'image', 'age', 'gender']
