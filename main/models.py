299b68a698d417fe45346c9b41f6827a933517ab
kaziSariaRahman <sariarahaman@gmail.com>
2024-11-28 22:29:07 +0600

    Added Donation class in Models.py

--- main/models.py
+++ main/models.py
@@ -1,8 +1,18 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class Donation(models.Model):
+    name = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    transaction_id = models.CharField(max_length=50)
+    #amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Optional donation amount
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Donation by {self.name} - {self.transaction_id}"
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     address = models.CharField(max_length=255)
@@ -10,3 +20,6 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+
